6d86d56f3071e01183c3f7183f061ba47b505301
hxq2291895932 <49118282+hxq2291895932@users.noreply.github.com>
2019-06-29 18:20:41 +0800

    修改版
    侯修全的修改版

--- HelloJavaFx/src/sample/Controller.java
+++ HelloJavaFx/src/sample/Controller.java
@@ -1,5 +1,32 @@
 package sample;
 
-public class Controller {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    private boolean check = true;
+    public Button login;
+    public Button sign;
+    public TextField myTextField;
+    public Label error;
+
+    public void setVisible(ActionEvent event) {
+        check = !check;
+        login.setDisable(check);
+        sign.setDisable(check);
+    }
+
+    public void sign(ActionEvent actionEvent) {
+        error.setVisible(true);
+    }
+
+    public void login(ActionEvent actionEvent) {
+        error.setVisible(true);
+    }
 }
--- HelloJavaFx/src/sample/Main.java
+++ HelloJavaFx/src/sample/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("MainStyle.css").toExternalForm());
+        primaryStage.setTitle("测试页面");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
